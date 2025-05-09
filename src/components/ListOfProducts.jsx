dbac1733faaf2e2e7517888337ba816641a9dbb8
Alex Pumari <alexpumari0@gmail.com>
2025-05-09 09:04:46 -0300

    Fix: Auto-fill problem in ListOfProducts

--- src/components/ListOfProducts.jsx
+++ src/components/ListOfProducts.jsx
@@ -1,10 +1,26 @@
+import { useContext } from 'react'
+import { ProductsContext } from '../contexts/products'
 import CardProduct from './CardProduct'
 
-export default function ListOfProducts({ products }) {
-  if (products?.length === 0) return (
-    <p className=''>Without products!</p>
-  )
+export default function ListOfProducts() {
+  const { products } = useContext(ProductsContext)
 
+  return products?.length === 0 ? (
+    <WithoutProducts />
+  ) : (
+    <><Products products={products} /><GhostProducts /></>
+  )
+}
+
+function WithoutProducts () {
+  return (
+    <p className='w-full flex items-center justify-center select-none'>
+      Without any products, you should create one!
+    </p>
+  )
+}
+
+function Products ({ products }) {
   return products.map(({ id, name, price, count }) => (
     <CardProduct
       key={id}
@@ -14,4 +30,15 @@ export default function ListOfProducts({ products }) {
       count={count}
     />
   ))
+}
+  
+function GhostProducts () {
+  // GhostProducts: add flex items to fix the auto-fill problem in flexbox
+  return (
+    <>
+      <div className='basis-44 flex-grow'></div>
+      <div className='basis-44 flex-grow'></div>
+      <div className='basis-44 flex-grow'></div>
+    </>
+  )
 }
