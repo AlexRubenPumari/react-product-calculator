9ac5038622b79266636fa962ed017454a4dee3c0
AlexRubenPumari <alexpumari0@gmail.com>
2025-06-17 13:06:34 -0300

    Fix: FileSelector and Modal components

--- src/components/common/FileSelector.jsx
+++ src/components/common/FileSelector.jsx
@@ -3,24 +3,24 @@ import { addArticleFor } from '../../logic/common/utilities'
 import { IconEdit, IconAdd } from './Icons'
 import ButtonCircle from './ButtonCircle'
 
-export default function FileSelector({ label, value, error, children, onFocus, className }) {
+export default function FileSelector({ label, value, error, children, onClick, className }) {
   const formatedTag = addArticleFor(label).toLowerCase()
-  const clonedChildren = cloneElement(children, { className: 'basis-32 flex-grow cursor-pointer', onClick: onFocus, ...value })
-  const classNameDiv = className + ' flex-center-all flex-wrap gap-4 relative'
+  const clonedChildren = cloneElement(children, { className: 'basis-32 max-w-48 flex-grow cursor-pointer', onClick: onClick, ...value })
+  const classNameDiv = className + ' flex-center-all flex-wrap gap-4 relative mb-8'
 
   return (
     <>
-      {label && <label htmlFor=''>{`Select ${formatedTag}:`}</label>}
+      {label && <span>{`Select ${formatedTag}:`}</span>}
       <div className={classNameDiv}>
         {value ? (
           clonedChildren
         ) : (
-          <WithoutFile formatedTag={formatedTag} onClick={onFocus} />
+          <WithoutFile formatedTag={formatedTag} onClick={onClick} />
         )}
         <ButtonCircle
           className='ButtonPrimary'
           title={`Choose ${formatedTag}`}
-          onClick={onFocus}>
+          onClick={onClick}>
           {
             value ? (
               <IconEdit className='size-4' />
@@ -38,7 +38,7 @@ export default function FileSelector({ label, value, error, children, onFocus, c
 function WithoutFile ({ formatedTag, onClick }) {
   return (
     <div
-      className='aspect-square basis-32 flex-grow flex-center-all rounded-lg bg-white cursor-pointer text-center'
+      className='aspect-square basis-32 max-w-48 flex-grow flex-center-all rounded-lg bg-white cursor-pointer text-center'
       onClick={onClick}
     >
       <span className='w-min text-gray-400'>
